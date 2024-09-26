4c908ef5dfb7d4bd975ec884c6e2dedd97109a13
oaoni <oaoni@loyola.edu>
2024-09-26 13:31:04 -0400

    Edited code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
+#Initialize the constant variables
+#Normal
+normal_height = 46
+normal_pm = 2
+normal_par = 90
+
+#Large
+large_height = 70
+large_pm = 1.9
+large_par = 120
+
+#Important variables that will be updated later in the program
+speed = 0
+
+#User Prompt and Opening message
+print("Ski Jump Success Calculator!")
+hillType = str(input("Enter the size of the hill you plan to ski on (please input either Large or Normal): "))
+
+if(hillType.lower == "normal"):
+    speed = int(input("Enter your speed: "))
